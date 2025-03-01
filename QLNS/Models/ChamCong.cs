9bbd388966587b99ef3fa62246ee04f090104135
PhongAnhChu <phonganhboy12@gmail.com>
2025-03-01 23:16:20 +0700

    Initial commit

---
+++ QLNS/Models/ChamCong.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QLNS.Models
+{
+    public class ChamCong
+    {
+        [Key]
+        public int MaChamCong { get; set; }
+
+        [ForeignKey("NhanVien")]
+        public int MaNhanVien { get; set; }
+        public NhanVien NhanVien { get; set; }
+
+        public DateTime NgayLam { get; set; }
+        public TimeSpan GioVaoLam { get; set; }
+        public TimeSpan GioKetThuc { get; set; }
+    }
+}
